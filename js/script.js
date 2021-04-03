9712f11784057c226612458b6fd2519b7b34084a
DESKTOP-8AT52HP\namhy <deokgu4624@gmail.com>
2021-04-03 15:25:33 +0900

    /

--- js/script.js
+++ js/script.js
@@ -2,8 +2,11 @@ var box = gsap.to("#blackbox2", {scale:15,ease:"bounce.inOut",duration: 3});
 box.pause();
 $("#blackbox2").css("display", "none");
 $("#mainimg").css("display", "none");
+$("#logo2").css("display", "none");
+$("#logo3").css("display", "none");
 
 var x=0;
+var test=true;
 function delay(){
     x+=1;
     console.log(x);
@@ -11,6 +14,13 @@ function delay(){
         clearInterval(timeid);
         $("#blackbox2").css("display", "block");
         $("#mainimg").css("display", "block");
+        $("#logo2").css("display", "block");
+        $("#logo3").css("display", "block");
+        $("#container").off();
+
+        gsap.fromTo("#logo2",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
+        gsap.fromTo("#logo3",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
+        gsap.fromTo("#mainimg",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
         box.play();
         
     }
