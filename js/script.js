41cebfcad07a83e1d99d94d781b895c2fced5a98
DESKTOP-8AT52HP\namhy <deokgu4624@gmail.com>
2021-04-04 02:18:49 +0900

    .

--- js/script.js
+++ js/script.js
@@ -4,6 +4,11 @@ $("#blackbox2").css("display", "none");
 $("#mainimg").css("display", "none");
 $("#logo2").css("display", "none");
 $("#logo3").css("display", "none");
+$("#article").css("display", "none");
+$("#article").css("display", "none");
+$("#star").css("display", "none");
+$("#star2").css("display", "none");
+$("#rose").css("display", "none");
 
 var x=0;
 var test=true;
@@ -12,15 +17,26 @@ function delay(){
     console.log(x);
     if(x==12){
         clearInterval(timeid);
+        $("#container").off();
         $("#blackbox2").css("display", "block");
         $("#mainimg").css("display", "block");
         $("#logo2").css("display", "block");
         $("#logo3").css("display", "block");
-        $("#container").off();
+        $("#article").css("display", "block");
+        $("#star").css("display", "block");
+        $("#star2").css("display", "block");
+        $("#rose").css("display", "block");
 
         gsap.fromTo("#logo2",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
-        gsap.fromTo("#logo3",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
-        gsap.fromTo("#mainimg",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
+        gsap.fromTo("#logo3",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
+        gsap.fromTo("#mainimg",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
+        gsap.fromTo("#article",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
+        gsap.fromTo("#star",{autoAlpha: 0, scale:0}, {autoAlpha: 1, scale: 1, ease:"bounce.out", duration: 1, delay: 3, repeat: -1, repeatDelay: 6});
+        gsap.fromTo("#star",{scale:1}, {scale: 0, ease: "back.in(1.7)", duration: 1, delay: 5, repeat: -1, repeatDelay: 6});
+        gsap.fromTo("#star2",{autoAlpha: 0, scale:0}, {autoAlpha: 1, scale: 1, ease:"bounce.out", duration: 1, delay: 4, repeat: -1, repeatDelay: 6});
+        gsap.fromTo("#star2",{scale:1}, {scale: 0, ease: "back.in(1.7)", duration: 1, delay: 6, repeat: -1, repeatDelay: 6});
+        gsap.to("body", {backgroundColor: "black", delay: 2});
+        gsap.fromTo("#rose",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
         box.play();
         
     }
