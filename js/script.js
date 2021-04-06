368be77183d3aa8466358e197f0caeb697656a17
DESKTOP-8AT52HP\namhy <deokgu4624@gmail.com>
2021-04-06 23:28:14 +0900

    .

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ $("#rose").css("display", "none");
 $("#time1").css("display", "none");
 $("#time2").css("display", "none");
 $("#area").css("display", "none");
+$("#play").css("display", "none");
 
 var x=0;
 var test=true;
@@ -33,7 +34,7 @@ function delay(){
         $("#time2").css("display", "block");
         $("#area").css("display", "block");
 
-        gsap.fromTo("#logo2",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
+        // gsap.fromTo("#logo2",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
         gsap.fromTo("#title",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
         gsap.fromTo("#mainimg",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
         gsap.fromTo("#article",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
@@ -42,9 +43,8 @@ function delay(){
         gsap.fromTo("#star2",{autoAlpha: 0, scale:0}, {autoAlpha: 1, scale: 1, ease:"bounce.out", duration: 1, delay: 4, repeat: -1, repeatDelay: 6});
         gsap.fromTo("#star2",{scale:1}, {scale: 0, ease: "back.in(1.7)", duration: 1, delay: 6, repeat: -1, repeatDelay: 6});
         gsap.to("body", {backgroundColor: "black", delay: 2});
-        gsap.fromTo("#rose",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
-        gsap.fromTo("#time1",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
-        gsap.fromTo("#time2",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
+        //gsap.fromTo("#time1",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
+        //gsap.fromTo("#time2",{autoAlpha: 0}, {autoAlpha: 1, top:"+=1"+"vw", duration: 1, delay: 2});
         gsap.to("#albumbox",{rotation:"360deg", ease:"none", duration: 6, delay: 2, repeat: -1});
         gsap.fromTo("#area",{autoAlpha: 0}, {autoAlpha: 1, duration: 1, delay: 2});
         box.play();
@@ -64,6 +64,17 @@ $("#container").mouseleave(function(){
     console.log("out");
     clearInterval(timeid);
     x=0;
+});
+
+var list = $("li");
+$("li").mouseenter(function(){
+    $(this).append($("#play"));
+    $("#play").css("display", "block");
+    gsap.fromTo("#play",{scale:0}, {scale: 1, ease:"bounce.out", duration: 1, delay: 0});
+});
+
+$("li").each(function(index, item){
+    $(item).css("top", index*3 +"vw");
 });
 
 
